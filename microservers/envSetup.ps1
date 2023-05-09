d371f1873653276809af278bc2ba76ed76b1c408
Rohit Wesley Thomas <wesleythomas360@gmail.com>
2023-05-09 00:49:40 -0700

    Added docker setup and launch automation scriptfor microservices

---
+++ microservers/envSetup.ps1
@@ -0,0 +1,32 @@
+# Set file paths
+$ENV_FILE=".env.example"
+$DOCKERIGNORE_FILE=".dockerignore"
+$GITIGNORE_FILE=".gitignore"
+
+# Create files for redis-microserver
+Write-Output "Setup redis-microserver"
+# Invoke-WebRequest -Uri https://raw.githubusercontent.com/redis/redis/6.2/redis.conf -OutFile redis-microserver/redis.conf
+Copy-Item -Path $ENV_FILE -Destination redis-microserver/.env -Force
+Copy-Item -Path $GITIGNORE_FILE -Destination redis-microserver/.gitignore -Force
+Copy-Item -Path $DOCKERIGNORE_FILE -Destination redis-microserver/.dockerignore -Force
+
+# # Create files for python-microserver
+# Write-Output "Setup python-microserver"
+# Copy-Item -Path $ENV_FILE -Destination python-microserver/.env -Force
+# Copy-Item -Path $GITIGNORE_FILE -Destination python-microserver/.gitignore -Force
+# Copy-Item -Path $DOCKERIGNORE_FILE -Destination python-microserver/.dockerignore -Force
+
+# # Create files for ffmpeg-microserver
+# Write-Output "Setup ffmpeg-microserver"
+# Copy-Item -Path $ENV_FILE -Destination ffmpeg-microserver/.env -Force
+# Copy-Item -Path $GITIGNORE_FILE -Destination ffmpeg-microserver/.gitignore -Force
+# Copy-Item -Path $DOCKERIGNORE_FILE -Destination ffmpeg-microserver/.dockerignore -Force
+
+# Create files for dashboard-microserver
+Write-Output "Setup dashboard-microserver"
+Copy-Item -Path $ENV_FILE -Destination dashboard-microserver/.env -Force
+Copy-Item -Path $GITIGNORE_FILE -Destination dashboard-microserver/.gitignore -Force
+Copy-Item -Path $DOCKERIGNORE_FILE -Destination dashboard-microserver/.dockerignore -Force
+
+# Display success message
+Write-Output "Created $ENV_FILE, $DOCKERIGNORE_FILE, and $GITIGNORE_FILE files."
---
+++ microservers/envSetup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Set file paths
+ENV_FILE=".env.example"
+DOCKERIGNORE_FILE=".dockerignore"
+GITIGNORE_FILE=".gitignore"
+
+# Create files for redis-microserver
+echo "Setup redis-microserver"
+# curl -sL https://raw.githubusercontent.com/redis/redis/6.2/redis.conf -o redis-microserver/redis.conf
+cp -f $ENV_FILE redis-microserver/.env
+cp -f $GITIGNORE_FILE redis-microserver/.gitignore
+cp -f $DOCKERIGNORE_FILE redis-microserver/.dockerignore
+
+# # Create files for python-microserver
+# echo "Setup python-microserver"
+# cp -f $ENV_FILE python-microserver/.env
+# cp -f $GITIGNORE_FILE python-microserver/.gitignore
+# cp -f $DOCKERIGNORE_FILE python-microserver/.dockerignore
+
+# # Create files for ffmpeg-microserver
+# echo "Setup ffmpeg-microserver"
+# cp -f $ENV_FILE ffmpeg-microserver/.env
+# cp -f $GITIGNORE_FILE ffmpeg-microserver/.gitignore
+# cp -f $DOCKERIGNORE_FILE ffmpeg-microserver/.dockerignore
+
+# Create files for dashboard-microserver
+echo "Setup dashboard-microserver"
+cp -f $ENV_FILE dashboard-microserver/.env
+cp -f $GITIGNORE_FILE dashboard-microserver/.gitignore
+cp -f $DOCKERIGNORE_FILE dashboard-microserver/.dockerignore
+
+# Display success message
+echo "Created $ENV_FILE, $DOCKERIGNORE_FILE, and $GITIGNORE_FILE files."
